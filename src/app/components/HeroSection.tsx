25dc87bd94bd3434be25a6406b42d98b9ce84320
Ali Sawari <ali.dragon.swr@gmail.com>
2025-07-18 20:59:32 +0300

    Update HeroSection images to reflect new phone interface visuals.

--- src/app/components/HeroSection.tsx
+++ src/app/components/HeroSection.tsx
@@ -6,16 +6,15 @@ import RotatingText from "./ui/RotatingText";
 import GridMotionBg from "./ui/GridMotionBg";
 
 const mobileImages = [
-  '/img/realistic-glassmorphism-mobile-app-template_23-2149441714.avif',
-  '/img/realistic-glassmorphism-mobile-app-template_23-2149441714.avif',
-  '/img/istockphoto-1418233376-612x612.jpg',
-  '/img/professional-programmer-working-late-dark-office.jpg',
-  '/img/4380747.jpg',
-  '/img/igor-omilaev-eGGFZ5X2LnA-unsplash.jpg',
-  '/img/side-shot-code-editor-using-react-js.jpg',
-  '/img/professional-programmer-working-late-dark-office.jpg',
-  '/img/4380747.jpg',
-  '/img/igor-omilaev-eGGFZ5X2LnA-unsplash.jpg'
+  '/img/phone/Onboarding.jpg',
+  '/img/phone/Notification.jpg',
+  '/img/phone/map-view.jpg',
+  '/img/phone/all-events.jpg',
+  '/img/phone/Filter.jpg',
+  '/img/phone/Event-Details.jpg',
+  '/img/phone/Home.jpg',
+  '/img/phone/Organizer-Profile-Event.jpg',
+  '/img/phone/music-share.png',
 ]
 
 export default function HeroSection() {
